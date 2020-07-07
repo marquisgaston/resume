a9c1da95133e33b50b43310fe60577c9068bcdc6
mjg1123 <marquisgbusiness@gmail.com>
2020-07-07 00:26:52 -0400

    fixed history in navbar error

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { Navbar } from 'react-bootstrap';
-import history from '../../history';
 
 import { connect } from 'react-redux';
 import * as actions from '../../actions';
