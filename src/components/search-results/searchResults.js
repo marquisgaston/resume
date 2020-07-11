1cf0f24cbc162815f10cf8330580969eb4ad839c
mjg1123 <marquisgbusiness@gmail.com>
2020-07-10 20:05:07 -0400

    removed axios for netlify error fix

--- src/components/search-results/searchResults.js
+++ src/components/search-results/searchResults.js
@@ -2,8 +2,6 @@ import React, { Component } from 'react';
 
 import { connect } from 'react-redux';
 import * as actions from '../../actions';
-import axios from 'axios';
-
 import SearchPagination from './searchPagination';
 
 class SearchResultsPage extends Component {
